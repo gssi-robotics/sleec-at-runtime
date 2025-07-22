60a007aa1df076d800dc16a4a0ea6d08c2eb4862
SaraPettinari <sara.pettinari@unicam.it>
2025-07-22 17:36:21 +0200

    missing file

---
+++ ros2_ws/src/sleec_enforcer_subsystem/sleec_enforcer_subsystem/firefighter_comm_layer/model_structures.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import Optional
+from dataclasses import dataclass
+
+# === ENUMS ===
+class WindScale(str, Enum):
+    LIGHT = "LIGHT"
+    MODERATE = "MODERATE"
+    STRONG = "STRONG"
+
+class CapabilityID(str, Enum):
+    DONOTHING = "DONOTHING"
+    SOUNDALARM = "SOUNDALARM"
+    GOHOME = "GOHOME"
+
+class Capability(str, Enum):
+    GOHOME = "goHome"
+    SOUNDALARM = "soundAlarm"
+
+@dataclass
+class Probe:
+    batteryCritical: bool
+    cameraStart: bool
+    alarmRinging: bool
+    personNearby: bool
+    temperature: float
+    windSpeed: WindScale
+    alarmdeadline: int
+
+@dataclass
+class Obligation:
+    capability: Capability
+
+    def get_id(self) -> CapabilityID:
+        mapping = {
+            Capability.DONOTHING: CapabilityID.DONOTHING,
+            Capability.GOHOME: CapabilityID.GOHOME,
+            Capability.SOUNDALARM: CapabilityID.SOUNDALARM,
+        }
+        return mapping[self.capability]
